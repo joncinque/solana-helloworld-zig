2642171828252f889cc582358953f35a8458b869
Jon C <me@jonc.dev>
2024-02-26 02:02:23 +0100

    build: Run any unit tests in the program

--- build.zig
+++ build.zig
@@ -10,4 +10,12 @@ pub fn build(b: *std.build.Builder) !void {
         .target = sol.bpf_target,
     });
     try sol.buildProgram(b, program, "sol/");
+
+    const test_step = b.step("test", "Run unit tests");
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+    });
+    sol.addSolModules(b, unit_tests, "sol/");
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&run_unit_tests.step);
 }
