b7f1cad196621d222cc521ae42638f0091ded704
Jon C <me@jonc.dev>
2024-04-27 16:10:00 +0200

    Return an error in test scripts

--- program-test/test.sh
+++ program-test/test.sh
@@ -9,5 +9,8 @@ set -e
 for i in $(seq 1 5)
 do
   $ZIG build --summary all --verbose && break || sleep 1
+  if [[ $i == 5 ]]; then
+    exit 1
+  fi
 done
 SBF_OUT_DIR="$ROOT_DIR/zig-out/lib" cargo test --manifest-path "$ROOT_DIR/program-test/Cargo.toml"
