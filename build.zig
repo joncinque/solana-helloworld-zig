9c48a277095876b2b2855d52ffbddd8e6b14cad0
Jon C <me@jonc.dev>
2025-08-30 14:55:44 +0200

    Don't run unit tests under Solana

--- build.zig
+++ build.zig
@@ -27,8 +27,6 @@ pub fn build(b: *std.Build) !void {
     const test_step = b.step("test", "Run unit tests");
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
     });
     lib_unit_tests.root_module.addImport("solana_program_sdk", solana_mod);
     const run_unit_tests = b.addRunArtifact(lib_unit_tests);
