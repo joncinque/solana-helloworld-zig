159b707c3575cee2ae0fb438f84a89a8701473e2
Jon C <me@jonc.dev>
2025-08-30 15:09:54 +0200

    Update windows job

--- install-rust-build-deps.sh
+++ install-rust-build-deps.sh
@@ -2,8 +2,24 @@
 set -e
 case $(uname -s | cut -c1-7) in
 "Windows" | "MINGW64")
-  vcpkg install openssl:x64-windows-static-md
-  vcpkg integrate install
+  export "PERL=$(which perl)"
+  export "OPENSSL_SRC_PERL=$(which perl)"
+  cat > vcpkg.json <<EOL
+{
+  "dependencies": ["openssl"],
+  "overrides": [
+    {
+      "name": "openssl",
+      "version": "3.4.1"
+    }
+  ],
+  "builtin-baseline": "5ee5eee0d3e9c6098b24d263e9099edcdcef6631"
+}
+EOL
+  vcpkg install --triplet x64-windows-static-md
+  rm vcpkg.json
+  export "OPENSSL_LIB_DIR=$GITHUB_WORKSPACE/vcpkg_installed/x64-windows-static-md/lib"
+  export "OPENSSL_INCLUDE_DIR=$GITHUB_WORKSPACE/vcpkg_installed/x64-windows-static-md/include"
   choco install protoc
   export PROTOC='C:\ProgramData\chocolatey\lib\protoc\tools\bin\protoc.exe'
   ;;
